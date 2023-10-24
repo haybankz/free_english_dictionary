bc6de080c72e48eb67269351206f721482207cc4
Abeeb Oladele <oladele_abeeb@stearsng.com>
2023-10-24 23:56:08 +0100

    remove boiler plate code

--- example/free_dictionary_example.dart
+++ example/free_dictionary_example.dart
@@ -1,6 +1,6 @@
 import 'package:free_dictionary/free_dictionary.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  var awesome = FreeDictionary();
+  print('awesome: $awesome');
 }
--- test/free_dictionary_test.dart
+++ test/free_dictionary_test.dart
@@ -1,16 +1,13 @@
-import 'package:free_dictionary/free_dictionary.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
-
     setUp(() {
       // Additional setup goes here.
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      expect(true, isTrue);
     });
   });
 }
