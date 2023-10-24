8f294c2ae4bdd39f3f9f14f6362430f98b9f5ff6
Abeeb Oladele <oladele_abeeb@stearsng.com>
2023-10-24 23:53:57 +0100

    initial commit

---
+++ example/free_dictionary_example.dart
@@ -0,0 +1,6 @@
+import 'package:free_dictionary/free_dictionary.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/free_dictionary_test.dart
@@ -0,0 +1,16 @@
+import 'package:free_dictionary/free_dictionary.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
